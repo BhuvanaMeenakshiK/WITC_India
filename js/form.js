a5138028bbdaa37aecdbd9cf07bfb85a5fa224bf
vignesh03 <vignesh.wit2021@gmail.com>
2022-01-07 13:41:01 +0530

    fix(app) : Added alert for submission

--- js/form.js
+++ js/form.js
@@ -5,6 +5,8 @@ const form = document.forms["contact-form"];
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
+    .then((response) =>
+      alert("Recieved your query. We'll get back to you asap!")
+    )
     .catch((error) => console.error("Error!", error.message));
 });
